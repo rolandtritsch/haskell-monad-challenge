ef5ed433d8fe7115f33e1d8dde113ed1f6f2a82e
Roland Tritsch <roland@tritsch.org>
2019-01-11 15:46:14 +0000

    Set02: Adding queryGreek.

--- src/Set02.hs
+++ src/Set02.hs
@@ -1,3 +1,67 @@
 module Set02 where
 
+import Prelude hiding (Maybe, Nothing, Just)
+
 import MCPrelude
+
+data Maybe a = Just a | Nothing
+
+instance Show a => Show (Maybe a) where
+  show (Just a) = "Just " ++ (show a)
+  show Nothing = "Nothing"
+
+instance Eq a => Eq (Maybe a) where
+  (==) (Just a) (Just b) = a == b
+  (==) Nothing Nothing = True
+  (==) _ _ = False
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (a:_) = Just a
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (_:as) = Just as
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay key ((key', value'):rest)
+  | key == key' = Just value'
+  | otherwise = lookupMay key rest
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0 = Nothing
+divMay x y = Just (x / y)
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay as = Just (maximum as)
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay as = Just (minimum as)
+
+queryGreek :: GreekData -> String -> Maybe Double
+queryGreek database key = result where
+  result
+    | isNothing max' = Nothing
+    | isNothing head' = Nothing
+    | otherwise = divMay (fromIntegral $ fromJust max') (fromIntegral $ fromJust head')
+  max'
+    | isNothing tail' = Nothing
+    | otherwise = maximumMay (fromJust tail')
+  tail'
+    | isNothing values' = Nothing
+    | otherwise = tailMay (fromJust values')
+  head'
+    | isNothing values' = Nothing
+    | otherwise = headMay (fromJust values')
+  values' = lookupMay key database
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+fromJust :: Maybe a -> a
+fromJust (Just a) = a
+fromJust Nothing = error "fromJust: Nothing"
--- test/Set02Spec.hs
+++ test/Set02Spec.hs
@@ -2,12 +2,30 @@ module Set02Spec where
 
 import Test.Hspec
 
-import MCPrelude
+import Prelude hiding (Maybe(..))
 
-import Set02
+import MCPrelude (
+  greekDataA,
+  greekDataB
+  )
+
+import Set02 (
+  Maybe(..),
+  queryGreek
+  )
 
 run :: IO ()
 run = hspec $ do
-  describe "hello" $ do
-    it "should return the correct result" $ do
-      reverse "dnalor" `shouldBe` "roland"
+  describe "queryGreek" $ do
+    it "should return the correct result(s)" $ do
+      queryGreek greekDataA "alpha" `shouldBe` Just 2.0
+      queryGreek greekDataA "beta" `shouldBe` Nothing
+      queryGreek greekDataA "gamma" `shouldBe` Just 3.3333333333333335
+      queryGreek greekDataA "delta" `shouldBe` Nothing
+      queryGreek greekDataA "zeta" `shouldBe` Nothing
+
+      queryGreek greekDataB "rho" `shouldBe` Nothing
+      queryGreek greekDataB "phi" `shouldBe` Just 0.24528301886792453
+      queryGreek greekDataB "chi" `shouldBe` Just 9.095238095238095
+      queryGreek greekDataB "psi" `shouldBe` Nothing
+      queryGreek greekDataB "omega" `shouldBe` Just 24.0
