a5ba1fc62b37b5186f93394e187e619921e781f1
Roland Tritsch <roland@tritsch.org>
2019-01-13 09:02:52 +0000

    Checkpoint to rename the repo.

--- src/Set03.hs
+++ src/Set03.hs
@@ -27,3 +27,39 @@ instance Show Card where
 allCards :: [Int] -> [String] -> [Card]
 allCards ranks suites = map mkCard $ allPairs'' ranks suites where
   mkCard (r, s) = Card r s
+
+allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs mkComb (x:[]) ys = allComb mkComb x ys
+allCombs mkComb (x:xs) ys = allComb mkComb x ys ++ allCombs mkComb xs ys
+
+allComb :: (a -> b -> c) -> a -> [b] -> [c]
+allComb _ _ [] = []
+allComb mkComb x (y:ys) = mkComb x y : allComb mkComb x ys
+
+allPairs''' :: [a] -> [b] -> [(a,b)]
+allPairs''' xs ys = allCombs (,) xs ys
+
+allCards' :: [Int] -> [String] -> [Card]
+allCards' ranks suites = allCombs mkCard ranks suites where
+  mkCard r s = Card r s
+
+allCombs3 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3 mkComb (x:[]) ys zs = allComb3 mkComb x ys zs
+allCombs3 mkComb (x:xs) ys zs = allComb3 mkComb x ys zs ++ allCombs3 mkComb xs ys zs
+
+allComb3 :: (a -> b -> c -> d) -> a -> [b] -> [c] -> [d]
+allComb3 mkComb x (y:[]) zs = allComb3' mkComb x y zs
+allComb3 mkComb x (y:ys) zs = allComb3' mkComb x y zs ++ allComb3 mkComb x ys zs
+
+allComb3' :: (a -> b -> c -> d) -> a -> b -> [c] -> [d]
+allComb3' _ _ _ [] = []
+allComb3' mkComb x y (z:zs) = mkComb x y z : allComb3' mkComb x y zs
+
+combStep :: [a -> b] -> [a] -> [b]
+combStep fcombs xs = concatMap (\fcomb -> map fcomb xs) fcombs
+
+allCombs' :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs' mkComb = combStep . combStep [mkComb]
+
+allCombs3' :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3' mkComb = combStep . combStep [mkComb] . combStep [mkComb]
--- test/Set03Spec.hs
+++ test/Set03Spec.hs
@@ -13,10 +13,15 @@ run = hspec $ do
       allPairs [1,2] [3,4] `shouldBe` [(1,3),(1,4),(2,3),(2,4)]
       allPairs' [1,2] [3,4] `shouldBe` [(1,3),(1,4),(2,3),(2,4)]
       allPairs'' [1,2] [3,4] `shouldBe` [(1,3),(1,4),(2,3),(2,4)]
+      allPairs''' [1,2] [3,4] `shouldBe` [(1,3),(1,4),(2,3),(2,4)]
       allPairs [1..3] [6..8] `shouldBe` [(1,6),(1,7),(1,8),(2,6),(2,7),(2,8),(3,6),(3,7),(3,8)]
       allPairs' [1..3] [6..8] `shouldBe` [(1,6),(1,7),(1,8),(2,6),(2,7),(2,8),(3,6),(3,7),(3,8)]
       allPairs'' [1..3] [6..8] `shouldBe` [(1,6),(1,7),(1,8),(2,6),(2,7),(2,8),(3,6),(3,7),(3,8)]
+      allPairs''' [1..3] [6..8] `shouldBe` [(1,6),(1,7),(1,8),(2,6),(2,7),(2,8),(3,6),(3,7),(3,8)]
+      allPairs cardRanks cardSuits `shouldBe` [(2,"H"),(2,"D"),(2,"C"),(2,"S"),(3,"H"),(3,"D"),(3,"C"),(3,"S"),(4,"H"),(4,"D"),(4,"C"),(4,"S"),(5,"H"),(5,"D"),(5,"C"),(5,"S")]
+      allPairs' cardRanks cardSuits `shouldBe` [(2,"H"),(2,"D"),(2,"C"),(2,"S"),(3,"H"),(3,"D"),(3,"C"),(3,"S"),(4,"H"),(4,"D"),(4,"C"),(4,"S"),(5,"H"),(5,"D"),(5,"C"),(5,"S")]
       allPairs'' cardRanks cardSuits `shouldBe` [(2,"H"),(2,"D"),(2,"C"),(2,"S"),(3,"H"),(3,"D"),(3,"C"),(3,"S"),(4,"H"),(4,"D"),(4,"C"),(4,"S"),(5,"H"),(5,"D"),(5,"C"),(5,"S")]
+      allPairs''' cardRanks cardSuits `shouldBe` [(2,"H"),(2,"D"),(2,"C"),(2,"S"),(3,"H"),(3,"D"),(3,"C"),(3,"S"),(4,"H"),(4,"D"),(4,"C"),(4,"S"),(5,"H"),(5,"D"),(5,"C"),(5,"S")]
 
   describe "show" $ do
     it "should return the correct result(s)" $ do
@@ -25,3 +30,9 @@ run = hspec $ do
   describe "allCards" $ do
     it "should return the correct result(s)" $ do
       show (allCards cardRanks cardSuits) `shouldBe` "[2H,2D,2C,2S,3H,3D,3C,3S,4H,4D,4C,4S,5H,5D,5C,5S]"
+      show (allCards' cardRanks cardSuits) `shouldBe` "[2H,2D,2C,2S,3H,3D,3C,3S,4H,4D,4C,4S,5H,5D,5C,5S]"
+
+  describe "allCombs" $ do
+    it "should return the correct result(s)" $ do
+      allCombs (,) [1,2] [3,4] `shouldBe` [(1,3),(1,4),(2,3),(2,4)]
+      allCombs3 (,,) [1,2] [3,4] [5,6] `shouldBe` [(1,3,5),(1,3,6),(1,4,5),(1,4,6),(2,3,5),(2,3,6),(2,4,5),(2,4,6)]
