1834649b27991c21bdb919f39d787ac5a66c86ea
Roland Tritsch <roland@tritsch.org>
2019-01-13 12:06:19 +0000

    Set03: Adding combStep.

--- src/Set03.hs
+++ src/Set03.hs
@@ -9,12 +9,12 @@ allPairs' :: [a] -> [b] -> [(a,b)]
 allPairs' xs ys = concatMap (\x -> map (\y -> (x, y)) ys) xs
 
 allPairs'' :: [a] -> [b] -> [(a,b)]
-allPairs'' (x:[]) ys = allPair x ys
-allPairs'' (x:xs) ys = allPair x ys ++ allPairs xs ys
+allPairs'' (x:[]) ys = allPair'' x ys
+allPairs'' (x:xs) ys = allPair'' x ys ++ allPairs'' xs ys
 
-allPair :: a -> [b] -> [(a,b)]
-allPair _ [] = []
-allPair x' (y':ys') = (x', y') : allPair x' ys'
+allPair'' :: a -> [b] -> [(a,b)]
+allPair'' _ [] = []
+allPair'' x' (y':ys') = (x', y') : allPair'' x' ys'
 
 data Card = Card {
   rank :: Int,
@@ -58,8 +58,11 @@ allComb3' mkComb x y (z:zs) = mkComb x y z : allComb3' mkComb x y zs
 combStep :: [a -> b] -> [a] -> [b]
 combStep fcombs xs = concatMap (\fcomb -> map fcomb xs) fcombs
 
+combStep' :: [a] -> [a -> b] -> [b]
+combStep' = flip combStep
+
 allCombs' :: (a -> b -> c) -> [a] -> [b] -> [c]
-allCombs' mkComb = combStep . combStep [mkComb]
+allCombs' mkComb xs ys = combStep' ys $ combStep' xs [mkComb]
 
 allCombs3' :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
-allCombs3' mkComb = combStep . combStep [mkComb] . combStep [mkComb]
+allCombs3' mkComb xs ys zs = combStep' zs $ combStep' ys $ combStep' xs [mkComb]
