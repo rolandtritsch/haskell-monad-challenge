1708a5820e0d7f25bd3e08e17bd24c051c9db173
Roland Tritsch <roland@tritsch.org>
2019-01-13 12:18:41 +0000

    Set03: Fixing/Fleshing out example03.

--- app/Example03.hs
+++ app/Example03.hs
@@ -8,4 +8,7 @@ import Set03
 
 main :: IO ()
 main = do
-  printf "%s\n" (show $ allPairs'' [1, 2] [3, 4])
+  printf "%s\n" (show $ allPairs''' [1, 2] [3, 4])
+  printf "%s\n" (show $ allCards' cardRanks cardSuits)
+  printf "%s\n" (show $ allCombs' (,) [1,2] [3,4])
+  printf "%s\n" (show $ allCombs3' (,,) [1,2] [3,4] [5,6])
